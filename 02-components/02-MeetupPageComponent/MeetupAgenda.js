d685115194158ec7eb6ffe7cf68d8f3d6177134a
veremey <veremey4uk@gmail.com>
2020-11-24 00:37:21 +0200

    solution 02-components/02-MeetupPageComponent

--- 02-components/02-MeetupPageComponent/MeetupAgenda.js
+++ 02-components/02-MeetupPageComponent/MeetupAgenda.js
@@ -1,14 +1,40 @@
-// import { MeetupAgendaItem } from './MeetupAgendaItem.js';
+import { MeetupAgendaItem } from './MeetupAgendaItem.js';
+import { agendaItemIcons, agendaItemTitles } from './data.js';
 
 export const MeetupAgenda = {
   name: 'MeetupAgenda',
-
   template: `
     <div class="meetup-agenda">
-      <div class="meetup-agenda__item"></div>
+      <p v-if="agenda.length <= -1" class="meetup-agenda__empty">Программа пока пуста, но когда-нибудь в ней обязательно что-нибудь появится!</p>
+
+      <template v-else v-for="item, i in agenda" >
+        <meetup-agenda-item :agendaItem="agendaItem[i]"/>
+      </template>
     </div>`,
 
-  // components
+  components: {
+    MeetupAgendaItem,
+  },
 
-  // props
+  props: {
+    agenda: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  computed: {
+    agendaItem() {
+      return { ...this.agendaItemArray };
+    },
+
+    agendaItemArray() {
+      return this.agenda.map( (item) => ({
+        ...item,
+        title: item.title || agendaItemTitles[item.type],
+        icon: `/assets/icons/icon-${agendaItemIcons[item.type]}.svg`,
+      }));
+    },
+  },
+
 };
--- 02-components/02-MeetupPageComponent/MeetupDescription.js
+++ 02-components/02-MeetupPageComponent/MeetupDescription.js
@@ -1,5 +1,7 @@
 export const MeetupDescription = {
-  template: `<p class="meetup-description">Описание</p>`,
-
-  // Пропсы
+  name: 'MeetupDescription',
+  template: `<p class="meetup-description">{{description}}</p>`,
+  props: {
+    description: String,
+  },
 };
